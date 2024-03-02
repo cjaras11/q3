b836060e771486d519cf73b4c5611cd99bc896f3
unknown <fbthadmin@SD2-PW03KDEB.TLD-AD.COM>
2024-03-02 14:50:57 +0700

    Get staircase amount input from console and validate it.

--- src/com/cimb/assignment/q3/Q3Application.java
+++ src/com/cimb/assignment/q3/Q3Application.java
@@ -1,10 +1,51 @@
 package com.cimb.assignment.q3;
 
+import java.util.Scanner;
+
 public class Q3Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Please enter staircase amount.");
+		String staircaseAmount = scanner.nextLine();
+		
+		scanner.close();
+		
+		try {
+			new Q3Application().process(staircaseAmount);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
+	}
+	
+	private void process(String staircaseAmount) throws Exception {
+		
+		validateData(staircaseAmount);
+		
+	}
+	
+	private void validateData(String staircaseAmount) throws Exception {
+		
+		if (staircaseAmount == null || staircaseAmount.isBlank()) {
+			System.out.println("Staircase Amount cannot empty.");
+			throw new Exception("Invalid Data.");
+		}
+		
+		int iStaircaseAmount = 0;
+		
+		try {
+			iStaircaseAmount = Integer.parseInt(staircaseAmount);
+		} catch (NumberFormatException e) {
+			System.out.println("Staircase Amount must be a number.");
+			throw e;
+		}
+		
+		if (iStaircaseAmount < 1 || iStaircaseAmount > 45) {
+			System.out.println("Staircase Amount must be a number between 1 and 45.");
+		}
 	}
 
 }
